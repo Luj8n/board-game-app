ebbc67ed9bd3b76e9112c373c00bf9462ac347d1
Luj8n <l.milieska@gmail.com>
2020-11-30 21:10:05 +0200

    Added react-icons, styled search bar, css -> scss

--- src/App/index.js
+++ src/App/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Container from "../components/Container";
 import Header from "../components/Header";
 import Search from "../components/Search";
-import styles from "./main.module.css";
+import styles from "./main.module.scss";
 
 function App() {
   return (
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { searchGame } from "../../redux/actions";
 import { getGames } from "../../redux/selectors";
+import { IoSearchOutline } from "react-icons/io5";
 import styles from "./main.module.scss";
 
 function Search({ findGame, games }) {
@@ -11,16 +12,29 @@ function Search({ findGame, games }) {
     setInput(e.target.value);
   };
 
-  const queryInput = (e) => {
+  const queryInput = () => {
     findGame(input);
+    setInput("");
   };
 
   return (
     <div className={styles.search}>
-      <input type="text" className={styles.bar} value={input} onChange={handleInput}></input>
-      <button className={styles.button} onClick={queryInput}>
+      <input
+        type="text"
+        className={styles.searchBar}
+        value={input}
+        onChange={handleInput}
+        onKeyDown={(e) => {
+          // query results if space was pressed
+          if (e.key === "Enter") {
+            queryInput();
+          }
+        }}
+      ></input>
+      <div className={styles.button} onClick={queryInput}>
         Search
-      </button>
+        <IoSearchOutline className={styles.searchIcon} />
+      </div>
     </div>
   );
 }
