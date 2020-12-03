a8c8e12aa4bda99e5db5ccd7b2fd3da0af0f9f09
Luj8n <l.milieska@gmail.com>
2020-12-03 21:00:55 +0200

    Better list

--- src/components/Listing/index.js
+++ src/components/Listing/index.js
@@ -4,8 +4,21 @@ import styles from "./main.module.scss";
 function Listing({ info }) {
   return (
     <div className={styles.listing}>
-      <img src={info.image_url} className={styles.image} alt=""></img>
-      <h2 className={styles.title}>{info.name}</h2>
+      <div className={styles.imageContainer}>
+        <img src={info.image_url} className={styles.image} alt=""></img>
+      </div>
+      <div className={styles.info}>
+        <span className={styles.title}>{info.name}</span>
+        <div className={styles.otherInfo}>
+          <span className={styles.price}>Price: {info.price == "0.00" ? "Unknown" : `${info.price}$`}</span>
+          <span className={styles.price}>
+            Recommended players: {info.min_players ? `${info.min_players} - ${info.max_players}` : `Unknown`}
+          </span>
+          <span className={styles.price}>
+            Average playtime: {info.min_playtime ? `${info.min_playtime} - ${info.max_playtime}` : `Unknown`}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
